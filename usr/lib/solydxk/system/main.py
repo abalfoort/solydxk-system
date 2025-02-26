2632d21681c758f4e51ac25da4d19289c1b3af44
abalfoort <arjenbalfoort@hotmail.com>
2025-02-26 11:44:25 +0100

    Remove debug code

--- usr/lib/solydxk/system/main.py
+++ usr/lib/solydxk/system/main.py
@@ -30,25 +30,7 @@ scriptDir = abspath(dirname(__file__))
 
 def uncaught_excepthook(*args):
     sys.__excepthook__(*args)
-    if __debug__:
-        from pprint import pprint
-        from types import BuiltinFunctionType, ClassType, ModuleType, TypeType
-        tb = sys.last_traceback
-        while tb.tb_next:
-            tb = tb.tb_next
-        print(('\nDumping locals() ...'))
-        pprint({k:v for k,v in tb.tb_frame.f_locals.items()
-                    if not k.startswith('_') and
-                       not isinstance(v, (BuiltinFunctionType,
-                                          ClassType, ModuleType, TypeType))})
-        if sys.stdin.isatty() and (sys.stdout.isatty() or sys.stderr.isatty()):
-            try:
-                import ipdb as pdb  # try to import the IPython debugger
-            except ImportError:
-                import pdb as pdb
-            print(('\nStarting interactive debug prompt ...'))
-            pdb.pm()
-    else:
+    if not __debug__:
         details = '\n'.join(traceback.format_exception(*args)).replace('<', '').replace('>', '')
         title = _('Unexpected error')
         msg = _('SolydXK System Settings has failed with the following unexpected error. Please submit a bug report!')
